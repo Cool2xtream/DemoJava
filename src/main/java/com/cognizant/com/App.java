d24c4931af8b14635a2c4fafc3c280a31619fab3
arpit <arpitagarwalrocks@gmail.com>
2016-01-07 22:08:42 +0530

    done

---
+++ src/main/java/com/cognizant/com/App.java
@@ -0,0 +1,11 @@
+package com.cognizant.com;
+
+
+public class App 
+{
+    public static int sum(int x,int y)
+{
+
+  return x+y;
+}
+}
---
+++ src/test/java/com/cognizant/com/AppTest.java
@@ -0,0 +1,13 @@
+package com.cognizant.com;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AppTest 
+{
+@Test
+public void testIt()
+{
+int a=App.sum(2,2);
+   assertEquals(4,a);
+}
+}
